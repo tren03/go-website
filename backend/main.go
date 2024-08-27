abdb4f467ac4dfb51d4c95dcacf6be12fdb2a732
vishnu <vishnusethuraman@gmail.com>
2024-08-27 13:09:59 +0530

    added time to the posts

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
@@ -16,6 +17,8 @@ type Post struct{
 	Title string `json:"Title"`
 	Url string `json:"Url"`
 	Body string `json:"Body"`
+	Date  time.Time `json:"Date"`
+	
 }
 
 
@@ -26,11 +29,13 @@ type Post struct{
 
 func main(){
 	// Sample Post 1
+
 	post1 := Post{
 		ID:    1,
 		Title: "Introduction to Go",
 		Url:   "https://cpu.land",
 		Body:  "Go is an open-source programming language designed for simplicity and efficiency. In this post, we will cover the basics of Go, including its syntax, types, and how to write a simple Go program.",
+		Date: time.Now(),
 	}
 	//Sample Post 2
 	post2 := Post{
@@ -38,12 +43,14 @@ func main(){
 		Title: "Understanding HTTP in Go",
 		Url:   "http://antirez.com/news/108",
 		Body:  "In this post, we will explore how to build HTTP servers in Go. We will look at how to create routes, handle requests, and return responses. By the end, you'll have a good understanding of how to work with HTTP in Go.",
+		Date: time.Now(),
 	}
 
 	posts:=[]Post{post1,post2}
 
 	// chi router to router http requests
 	router:=chi.NewRouter()	
+
 	// cors config
 	router.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"https://*", "http://*"},
@@ -61,8 +68,11 @@ func main(){
 	// handles /posts
 	router.Get("/posts",handleViewPosts(posts))
 
+	
+
 
 	http.ListenAndServe(":8080", router)
+
 
 
 
